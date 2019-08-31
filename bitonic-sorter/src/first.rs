fad27e26413a6dc3a0c389635d33971637e096aa
unknown <yamashita-kenngo@outlook.jp>
2019-08-31 22:11:16 +0900

    sort関数実装

--- bitonic-sorter/src/first.rs
+++ bitonic-sorter/src/first.rs
@@ -1,5 +1,10 @@
 pub fn sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
+    }
 }
 // パブリックにした関数　中身は未実装
 
