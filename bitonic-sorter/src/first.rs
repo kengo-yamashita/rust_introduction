3bdce24dccb6b28fb73242c5226c26c8f2e51ae8
unknown <yamashita-kenngo@outlook.jp>
2019-08-31 22:00:55 +0900

    first module 仮実装

---
+++ bitonic-sorter/src/first.rs
@@ -0,0 +1,14 @@
+pub fn sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+// パブリックにした関数　中身は未実装
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+// プライベート関数　中身は未実装
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+// プライベート関数　中身は未実装
